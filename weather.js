e7fc47acf2735f45e3c5590c4e678678bb479063
doozi316 <doozi316@gmail.com>
2019-10-18 23:08:38 +0900

    날씨, 위치 알림 기능 추가

--- weather.js
+++ weather.js
@@ -1,4 +1,19 @@
+const weather = document.querySelector(".js-weather");
+const API_KEY = "a87542164980187d73bcc64f3121fa13";
 const COORDS = "coords";
+
+
+
+function getWeather(lat, lng){
+    fetch(`https://api.openweathermap.org/data/2.5/weather?lat=${lat}&lon=${lng}&appid=${API_KEY}&units=metric`
+    ).then(function(response){
+       return response.json();
+    }).then(function(json){
+        const temperature = json.main.temp;
+        const place = json.name;
+        weather.innerText = `${temperature} @ ${place}`;
+    });
+}
 
 function saveCoords(coordsObj){
     localStorage.setItem(COORDS, JSON.stringify(coordsObj));
@@ -16,6 +31,7 @@ function handleGeoSuccess(position){
         longitude: longitude
     };
     saveCoords(coordsObj);
+    getWeather(latitude, longitude);
 }
 
 function askForCoords(){
@@ -27,7 +43,8 @@ function loadCoords(){
     if(loadedCoords===null){
         askForCoords();
     }else{
-        //get Weather
+        const parsedCoords = JSON.parse(loadedCoords)
+        getWeather(parsedCoords.latitude, parsedCoords.longitude);
     }
 }
 
